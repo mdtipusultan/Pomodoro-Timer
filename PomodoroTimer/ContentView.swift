b6bcc627500e988763a695f5ffd3e64967bff7e1
Md. Tipu <tipu.diu.cse@gmail.com>
2026-06-18 16:31:09 +0600

    project initial
    project initial

---
+++ PomodoroTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PomodoroTimer
+//
+//  Created by MacBook Pro M1 Pro on 6/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
